ea5802e3542fef0a056ed240967851478d38a757
Lakshman Veti <lakshman.veti@gmail.com>
2022-07-11 16:20:10 +0530

    minor fix

--- src/App.ts
+++ src/App.ts
@@ -2,4 +2,5 @@ import * as _ from 'lodash';
 import {addDOMListener} from './Events';
 
 
-console.log(addDOMListener({element:'div'},null))
+console.log(addDOMListener({ element: 'div' }, null));
+console.log(_.cloneDeep({ x: 100, y: 100 }));
--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
       {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        // exclude: /node_modules/,
+        exclude: /node_modules/,
       },
     ],
   },
@@ -21,7 +21,7 @@ module.exports = {
     // alias: {
     //     handlebars: 'handlebars/dist/handlebars.js',
     // },
-    // modules: ['node_modules'],
+    modules: ['node_modules'],
     extensions: ['.ts', '.tsx', '.js']
   },
   optimization: {
